7c9725ee51ed546a5ebe93e8008f85ad39bdf0e7
Rohan <rohan@digithinkit.com>
2017-02-03 17:51:40 +0530

    Additions to invoice.py

--- library_management/doctype/invoice/invoice.py
+++ library_management/doctype/invoice/invoice.py
@@ -7,4 +7,11 @@ import frappe
 from frappe.model.document import Document
 
 class Invoice(Document):
-	pass
+	def on_submit(self):
+		date = frappe.get_value("Library Transaction", self.transaction_date, "transaction_date")
+		content = "Invoice generated for {0} issued to {1} on {2}".format(self.article_name, self.member_first_name, date)
+
+		frappe.sendmail(recipients=[self.email_id],
+			sender="test@library.com",
+			subject="Invoice details",
+			content=content)
